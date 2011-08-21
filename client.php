64da74c8232ee84c6fd38e9e5d2f63094359b9f3
Adarsh J <adarshaj@whirlpool.security.iitk.ac.in>
2011-08-21 10:00:33 +0530

    more json fixes

--- client.php
+++ client.php
@@ -154,7 +154,7 @@ YUI().use('tabview', 'escape', 'plugin', function(Y) {
    var cb = Y.one('#slac_contactbox');
    var dum = Y.one('#dummy');
    var counter_i = 0;
-   var contacts_len = contacts_json.length;
+   var contacts_len = 2;
    for(var counter_i=0; counter_i<contacts_len;++counter_i){
    	console.log(contacts_json[counter_i]);
    var item = Y.Node.create('<div class="contact" name="'+contacts_json[counter_i]['contact']['id']+'" id="contact_'+counter_i+'" open="false"><span class="availability"></span>'+contacts_json[counter_i]['contact']['id']+'<br/><span class="status"><em>Status Here</em></span></div>');
