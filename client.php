c1a07c1997a84778930a728a21e7bf5bf43efcd5
Adarsh J <adarshaj@whirlpool.security.iitk.ac.in>
2011-08-21 13:44:29 +0530

    Removed stub for status

--- client.php
+++ client.php
@@ -155,7 +155,7 @@ YUI().use('tabview', 'escape', 'plugin', function(Y) {
 					var counter_i = 0;
 				   var contacts_len = contacts_json.length;
 				   for(var counter_i=0; counter_i<contacts_len;++counter_i){
-				   var item = Y.Node.create('<div class="contact" name="'+contacts_json[counter_i]['contact']['id']+'" id="contact_'+counter_i+'" open="false"><span class="availability"></span>'+contacts_json[counter_i]['contact']['id']+'<br/><span class="status">Status Here</span><hr /></div>');
+				   var item = Y.Node.create('<div class="contact" name="'+contacts_json[counter_i]['contact']['id']+'" id="contact_'+counter_i+'" open="false"><span class="availability"></span>'+contacts_json[counter_i]['contact']['id']+'<br/><span class="status"></span><hr /></div>');
 				   
 				   
 				   cb.insertBefore(item,dum);
